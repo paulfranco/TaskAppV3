565ccc357a992d3348fadb0b7354d75619d32668
Paul Franco <mail@paulfran.co>
2021-02-28 21:13:30 -0800

    created project_row layout and item_row layout

--- app/src/main/java/co/paulfran/taskappv3/ItemActivity.kt
+++ app/src/main/java/co/paulfran/taskappv3/ItemActivity.kt
@@ -1,7 +1,8 @@
-package co.paulfran
+package co.paulfran.taskappv3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import co.paulfran.taskappv3.R
 
 class ItemActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
